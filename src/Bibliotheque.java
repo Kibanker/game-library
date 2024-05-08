1f54e813779a857a5de96472afd6e767f9565d03
Noa <noavincent17@gmail.com>
2024-05-08 15:34:18 +0200

    optimisation: la moitié des fonctions sont passés dans la classe Jeu (à modifier peut-etre)

--- src/Bibliotheque.java
+++ src/Bibliotheque.java
@@ -25,67 +25,10 @@ public class Bibliotheque { // Contrôleur
 
 		
 		f.setLayout(new FlowLayout());
-		
-//		final ImageIcon img = new ImageIcon("files/bg3.jpg");
-//		final ImageIcon imgScaled = new ImageIcon(img.getImage()
-//                .getScaledInstance(130,
-//                        180, Image.SCALE_SMOOTH));
-//		
-//		final JLabel label = new JLabel(imgScaled);
-//		
-//		f.add(label);
 
-		Color lightBlue = new Color(180, 220, 250);
-		
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBackground(lightBlue);
-	    f.setJMenuBar(menuBar);
-
-	    JMenu menu = new JMenu("Accueil");
-	    JMenu menu2= new JMenu("Catégories");
-	    menuBar.add(menu);
-	    menuBar.add(menu2);
-	    
-	    menu.setBackground(new Color(173, 216, 230)); // Bleu clair
-        menu2.setBackground(new Color(173, 216, 230)); // Bleu clair
-
-        // Ajouter les sous-menus
-        JMenu[] subMenus = {
-            new JMenu("Action"),
-            new JMenu("Aventure"),
-            new JMenu("Course"),
-            new JMenu("Réflexion"),
-            new JMenu("Simulation"),
-            new JMenu("Stratégie"),
-            new JMenu("Sport")
-        };
-        for (JMenu subMenu : subMenus) {
-            menu2.add(subMenu);
-            // Définir la couleur de fond pour les sous-menus
-            subMenu.setBackground(new Color(173, 216, 230)); // Bleu clair
-        }
-
-
-//		ArrayList<Jeu> biblio = new ArrayList<Jeu>();
 //		
-//		
-//		// resume nom categorie date entreprise noteG noteC image media son nomination
-//		Jeu baldursGate = new Jeu("viande", "viande", "viande", "19/04/04", "viande", 5, 5, "files/bg3.jpg", null, null, null);
-//		Jeu eldenRing = new Jeu("viande", "viande", "viande", "19/04/04", "viande", 5, 5, "files/eldenring.jpeg", null, null, null);
-//		Jeu eldenRingg = new Jeu("viande", "viande", "viande", "19/04/04", "viande", 5, 5, "files/eldenring.jpeg", null, null, null);
-//		
-//		biblio.add(baldursGate);
-//		biblio.add(eldenRing);
-//		biblio.add(eldenRingg);
-//		
-//		for (int i = 0; i < biblio.size(); i++) {
-//			final JLabel label = new JLabel(biblio.get(i).image);
-//			
-//			f.add(label);
-//		}
-		
-		f.setSize(512*3, 512*3);
-		f.setVisible(true);
+//		f.setSize(512*3, 512*3);
+//		f.setVisible(true);
 		
 		ArrayList<Jeu> biblio = readGamesFromCSV("files/games.csv");
         SwingUtilities.invokeLater(() -> new Vue(biblio));
@@ -126,14 +69,14 @@ public class Bibliotheque { // Contrôleur
 	    try {
 	        noteGen = Double.parseDouble(data[5]); // Convertir en double
 	    } catch (NumberFormatException e) {
-	        noteGen = 0.0; // Gérer les valeurs non valides en attribuant une valeur par défaut
+	        noteGen = -1; // Gérer les valeurs non valides en attribuant une valeur par défaut
 	    }
 	    
 	    double noteCrea;
 	    try {
 	        noteCrea = Double.parseDouble(data[6]); // Convertir en double
 	    } catch (NumberFormatException e) {
-	        noteCrea = 0.0; // Gérer les valeurs non valides en attribuant une valeur par défaut
+	        noteCrea = -1; // Gérer les valeurs non valides en attribuant une valeur par défaut
 	    }
 	    
 	    String img = data[7];
