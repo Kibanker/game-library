6e418ca6a32d05af2651fd744f15da0f93a8d20b
julesdmt <julesdemont11@gmail.com>
2024-05-13 18:09:04 +0200

    ajout possible vote

--- src/DescriptionJeuPanel.java
+++ src/DescriptionJeuPanel.java
@@ -1,21 +1,19 @@
+import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.Border;
-import javax.swing.border.TitledBorder;
-
-import java.awt.*;
 
 public class DescriptionJeuPanel extends JPanel {
     private Jeu jeu;
+    private boolean voted; // Variable pour suivre si l'utilisateur a déjà voté pour ce jeu
     String noteCreaNA;
 
     public DescriptionJeuPanel(Jeu jeu) {
         this.jeu = jeu;
+        voted = false; // Initialiser voted à false pour chaque jeu
         setLayout(new BorderLayout());
         setBackground(Color.BLACK);
-		/*
-		 * setBorder(createGameBorder(jeu.nom)); setBackground(Color.BLACK);
-		 */
-        
+        setBorder(createGameBorder(jeu.nom));
+
         JPanel nomPanel = new JPanel(new BorderLayout());
         nomPanel.setBackground(Color.BLACK);
         JLabel nameLabel = new JLabel(jeu.nom);
@@ -24,17 +22,15 @@ public class DescriptionJeuPanel extends JPanel {
         nameLabel.setBorder(BorderFactory.createEmptyBorder(20, 20, 10, 20)); // Ajouter des marges au label du nom
         nomPanel.add(nameLabel);
         add(nomPanel, BorderLayout.NORTH);
-        
-        
 
         // Image du jeu en haut à gauche
         JLabel imageLabel = new JLabel(jeu.image);
         imageLabel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20)); // Ajouter des marges à l'image
         imageLabel.setBackground(Color.BLACK);
         imageLabel.setOpaque(true);
-        imageLabel.setBorder(createGameBorder(jeu.nom));
+        imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(imageLabel, BorderLayout.WEST);
-        
+
         JPanel resumePanel = new JPanel(new BorderLayout());
         resumePanel.setOpaque(false); // Permet au fond d'écran de s'afficher
         add(resumePanel, BorderLayout.CENTER);
@@ -63,15 +59,15 @@ public class DescriptionJeuPanel extends JPanel {
         descriptionPanel.setBackground(Color.BLACK);
 
         JTextArea descriptionArea = new JTextArea();
-        if(jeu.noteCrea == -1.0) {
-        	noteCreaNA = "N/A";
+        if (jeu.noteCrea == -1.0) {
+            noteCreaNA = "N/A";
         }
         descriptionArea.setText("Catégorie: " + jeu.categorie + "\n" +
                 "Date de sortie: " + jeu.dateDeSortie + "\n" +
                 "Entreprise: " + jeu.entreprise + "\n" +
                 "Note générale: " + jeu.noteGen + "\n" +
                 "Note de création: " + (jeu.noteCrea == -1.0 ? noteCreaNA : "N/A") + "\n" +
-                "Nominations: " + (jeu.nominations == "null" ? jeu.nominations : "N/A"));
+                "Nominations: " + (jeu.nominations.equals("null") ? "N/A" : jeu.nominations));
         descriptionArea.setForeground(Color.WHITE);
         descriptionArea.setBackground(Color.BLACK);
         descriptionArea.setFont(new Font("Arial", Font.PLAIN, 18));
@@ -79,11 +75,54 @@ public class DescriptionJeuPanel extends JPanel {
         descriptionArea.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20)); // Ajouter des marges à la zone de texte
         descriptionPanel.add(descriptionArea, BorderLayout.CENTER);
 
+        // Ajouter des étoiles pour permettre à l'utilisateur de donner une note
+        JPanel ratingPanel = new JPanel();
+        ratingPanel.setBackground(Color.BLACK);
+        ratingPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        JLabel ratingLabel = new JLabel("Donner une note: ");
+        ratingLabel.setForeground(Color.WHITE);
+        ratingPanel.add(ratingLabel);
+
+        // Créer des boutons d'étoile pour permettre à l'utilisateur de donner une note
+        for (int i = 1; i <= 5; i++) {
+            final int rating = i; // Déclarer la variable finale pour être utilisée dans l'expression lambda
+            ImageIcon starIcon = new ImageIcon("files/star.png"); // Remplacez "star.png" par le chemin de votre icône d'étoile
+            Image image = starIcon.getImage();
+            Image newImage = image.getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH);
+            ImageIcon newStarIcon = new ImageIcon(newImage);
+            JButton starButton = new JButton(newStarIcon);
+            starButton.setBorderPainted(false); // Supprimer la bordure du bouton
+            starButton.setContentAreaFilled(false); // Supprimer le remplissage du bouton
+            starButton.setFocusPainted(false); // Supprimer la mise au point du bouton
+            starButton.setOpaque(false); // Rendre le bouton transparent
+            starButton.addActionListener(e -> {
+                // Vérifier si l'utilisateur a déjà voté pour ce jeu
+                if (!voted) {
+                    // Mettre à jour la note dans la description du jeu
+                    descriptionArea.append("\nNote donnée par l'utilisateur: " + rating);
+                    // Désactiver les boutons d'étoiles après que l'utilisateur a voté
+                    for (Component component : ratingPanel.getComponents()) {
+                        if (component instanceof JButton) {
+                            ((JButton) component).setEnabled(false);
+                        }
+                    }
+                    voted = true; // Mettre à jour l'état du vote à true
+                } else {
+                    JOptionPane.showMessageDialog(this, "Vous avez déjà voté pour ce jeu.", "Vote déjà effectué", JOptionPane.WARNING_MESSAGE);
+                }
+            });
+            ratingPanel.add(starButton);
+        }
+
+        descriptionPanel.add(ratingPanel, BorderLayout.SOUTH);
+
         add(descriptionPanel, BorderLayout.EAST);
     }
-    
+
     private Border createGameBorder(String name) {
         Border emptyBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
         return BorderFactory.createCompoundBorder(new DropShadowBorder(), emptyBorder);
     }
 }
+
--- src/Jeu.java
+++ src/Jeu.java
@@ -1,46 +1,39 @@
 import java.awt.Image;
-
 import javax.swing.ImageIcon;
 
 public class Jeu { // Modèle
-	
-	// Attributs généraux
-	String resume;
-	String nom;
-	String categorie;
-	String dateDeSortie;
-	String entreprise;
-	ImageIcon image;
-	
-	
-	double noteGen; // note générale
-	double noteCrea; // notre note (nos notes?)
-	
-	// Attributs éventuels
-	String mediaAssocie; //  film ou serie ou anime
-	String son;
-	String nominations;
-	
-	public Jeu(String resume, String nom, String cat, String date, String entreprise, double noteG, double noteC, String img, String media, String son, String nominations) {
-		this.resume = resume;
-		this.nom = nom;
-		this.categorie = cat;
-		this.dateDeSortie = date;
-		this.entreprise = entreprise;
-		this.noteGen = noteG;
-		this.noteCrea = noteC;
-		
-		ImageIcon image = new ImageIcon(img);
-		this.image = new ImageIcon(image.getImage().getScaledInstance(130*2, 180*2, Image.SCALE_SMOOTH));
-		
-		this.mediaAssocie = media;
-		this.son = son;
-		this.nominations = nominations;
-	}
-	
-	
-	
-	
-	
-	
+    
+    // Attributs généraux
+    String resume;
+    String nom;
+    String categorie;
+    String dateDeSortie;
+    String entreprise;
+    ImageIcon image;
+    double noteGen; // note générale
+    double noteCrea; // note de création
+    double noteUtilisateur; // note donnée par l'utilisateur
+    
+    // Attributs éventuels
+    String mediaAssocie; // film, série ou anime associé
+    String son;
+    String nominations;
+    
+    public Jeu(String resume, String nom, String cat, String date, String entreprise, double noteG, double noteC, String img, String media, String son, String nominations) {
+        this.resume = resume;
+        this.nom = nom;
+        this.categorie = cat;
+        this.dateDeSortie = date;
+        this.entreprise = entreprise;
+        this.noteGen = noteG;
+        this.noteCrea = noteC;
+        this.noteUtilisateur = -1; // Initialiser la note utilisateur à -1 par défaut
+        
+        ImageIcon image = new ImageIcon(img);
+        this.image = new ImageIcon(image.getImage().getScaledInstance(130*2, 180*2, Image.SCALE_SMOOTH));
+        
+        this.mediaAssocie = media;
+        this.son = son;
+        this.nominations = nominations;
+    }
 }
